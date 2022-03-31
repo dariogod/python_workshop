0392a7c239903b6a1f1b2a6634881cb33d262b79
dario <dario.god@outlook.be>
2022-03-31 17:11:25 +0200

    pong start

--- Pong/pong.py
+++ Pong/pong.py
@@ -2,128 +2,94 @@ import turtle
 import os
 import time
 
+### Variables ###
+
+# Game Loop
 running = True
 
+# Score
+# TODO
+
+
+### Turtles ###
+
+# Window
 wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-# Score
-score_a = 0
-score_b = 0
-
 # Paddle A
-paddle_a = turtle.Turtle()
-paddle_a.speed(0)
-paddle_a.shape("square")
-paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=5,stretch_len=1)
-paddle_a.penup()
-paddle_a.goto(-350, 0)
+# TODO
 
 # Paddle B
 # TODO
 
 # Ball
-ball = turtle.Turtle()
-ball.speed(0)
-ball.shape("square")
-ball.color("white")
-ball.penup()
-ball.goto(0, 0)
-ball.dx = 1
-ball.dy = 1
-
-# Pen
-pen = turtle.Turtle()
-pen.speed(0)
-pen.shape("square")
-pen.color("white")
-pen.penup()
-pen.hideturtle()
-pen.goto(0, 260)
-pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
-
-# Tekst met instructie om spel af te sluiten
 # TODO
 
-# Functions
+# Text: Score
+# TODO
+
+
+### Functions ###
+
 def paddle_a_up():
-    #TODO
+    # TODO
     pass
 
 def paddle_a_down():
-    #TODO
+    # TODO
     pass
 
 def paddle_b_up():
-    #TODO
+    # TODO
     pass
 
 def paddle_b_down():
-    #TODO
+    # TODO
     pass
 
 def quit():
     global running
     running = False
 
-# Keyboard bindings
-wn.listen()
-wn.onkeypress(paddle_a_up, "z")
-wn.onkeypress(paddle_a_down, "s")
-wn.onkeypress(paddle_b_up, "Up")
-wn.onkeypress(paddle_b_down, "Down")
-wn.onkeypress(quit, "q")
 
-# Main game loop
-while running:
-    time.sleep(0.005)
-    
+### Keyboard bindings ###
+
+wn.listen()
+wn.onkeypress(quit, 'q')
+
+
+### Main game loop ###
+
+while running:    
     wn.update()
     
     # Move the ball
-    ball.setx(ball.xcor() + ball.dx)
-    ball.sety(ball.ycor() + ball.dy)
+    # TODO
 
-    # Border checking
+    ## Border checking ##
 
-    # Top and bottom
-    if ball.ycor() > 290:
-        ball.sety(290)
-        ball.dy *= -1
-        file = 'bounce.wav&'
-        os.system("aplay -q " + file)
+    # Top
+    # TODO
+
+    # Bottom
+    # TODO
+
+    # Left
+    # TODO
+
+    # Right
+    # TODO
+
+    ## Paddle and ball collisions ##
+
+    # Left
+    # TODO
     
-    elif ball.ycor() < -290:
-        ball.sety(-290)
-        ball.dy *= -1
-        os.system("aplay -q bounce.wav&")
-
-    # Left and right
-    if ball.xcor() > 400:
-        score_a += 1
-        pen.clear()
-        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
-        ball.goto(0, 0)
-        ball.dx *= -1
-
-    elif ball.xcor() < -400:
-        score_b += 1
-        pen.clear()
-        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
-        ball.goto(0, 0)
-        ball.dx *= -1
-
-    # Paddle and ball collisions
-    if ball.dx < 0 and ball.xcor() < -330 and ball.xcor() > -350 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
-        ball.dx *= -1 
-        os.system("aplay -q bounce.wav&")
-    
-    elif ball.dx > 0 and ball.xcor() > 330 and ball.xcor() < 350 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
-        ball.dx *= -1
-        os.system("aplay -q bounce.wav&")
+    # Right
+    # TODO
 
 wn.bye()
